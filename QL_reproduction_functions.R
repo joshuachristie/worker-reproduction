6a2a383f8b7dee97fbc1da178c2fdc2caca18c8a
Joshua <joshua@dhcp-wlan-uzh-10-12-202-187.uzh.ch>
2018-11-20 10:37:07 +0100

    rewrite generationNewPopulation function
    for each existing colony I need to check a couple of things
    is it QR? If so, does it
       remain QR or become QL? If remain QR, copy exactly; if become QL, change indicator to QL.
       how many daughter swarms does it produce?
       if it produces daughter swarms, do they stay QR or become QL
    if they stay QR, I can choose their nuclear alleles but not their spermathecal contents and set their indicator
    if they become QL, their nuclear alleles and spermathecal contents become that of the daughter queen's mother (since the workers in the QL swarm are not from the daughter queen, given that she died on her mating flight/quest to find a home
    I assume that neither QR or QL "new" daughter swarms produce drones in the generation in which they are produced (they are busy setting up home)
    
    I've reorganised this function so it proceeds in a more logical
    manner. Previously, the various "actions" that could happen to a
    colony were separated, which led to redundancy in code and made it
    very difficult to follow

--- QL_reproduction_functions.R
+++ QL_reproduction_functions.R
@@ -129,133 +129,6 @@ testAgainstRandomNumber <- function(threshold){
 
 }
 
-    
-                                        #determine which colonies survive intact (queen + colony)
-    queenright_survival <- runif(N) < #depends on fitness of colony AND on population size
-        (prob_queen_survives * population[,number_alleles + 3]) 
-                                        #determine which colonies survive without queen
-    queenless_survival <- rep(TRUE,N)
-    
-    queen_survival[queenright_survival] <- 1 #colony survives queenright
-    queen_survival[!queenright_survival & !queenless_survival] <- -1 #colony dies
-                                        #remainder are !queenright_survival & queenless_survival, which store 0
-    
-###queen_survival[ii] reads 1 when iith colony is QR, 0 when QL, -1 when dead###  
-    
-                                        #determine the size of the next generation (daughter swarms of QR parents ONLY + all parent QR colonies that survive (as QR or QL))
-    N_new_generation <- N_daughter_colonies + sum((population[,number_alleles + 4] == 1 & queen_survival > -1))
-    
-                                        #at this point, the population can go extinct. If it does, I want to escape the function and
-                                        # repeat the foreach loop in the main script (so that I get 500 simulations in which the population does not go extinct).
-    ## ADD HERE A CONDITION THAT THERE ARE QR COLONIES (OTHERWISE POPULATION WILL GO EXTINCT THE NEXT GENERATION)
-    if (N_new_generation == 0){ #population is extinct 
-        
-        population <- 'extinct' #for conditional statement in main script
-        return(population) #exit function
-        
-    } else { #population is not extinct, proceed normally
-        
-                                        #initialise arrays
-        new_population <- matrix(numeric(N_new_generation * (2 + number_alleles + 2)),
-                                 nrow = N_new_generation, ncol = 2 + number_alleles + 2)
-        distribution_alleles <- numeric(number_alleles)
-
-        ## If a new colony goes QL, I need to be able to track which colony it came from
-        population_original_queen_index <- numeric(N_new_generation)
-
-        for (ii in 1:N){
-                                        #is colony ii QR? If so, does it remain QR?
-            if (queen_survival[ii] == 1 && population[ii,number_alleles + 4] == 1){ 
-                
-                new_population[swarm_counter,] <- population[ii,] #if TRUE, copy colony ii exactly
-
-                ## store which colony this entry is derived from
-                population_original_queen_index[swarm_counter] <- ii
-
-                swarm_counter <- swarm_counter + 1 
-                
-                                        #QR colony produces drones
-                distribution_alleles[population[ii,1:2]] <- distribution_alleles[population[ii,1:2]] + 
-                    (population[ii,number_alleles + 3] * 0.5) 
-                
-                                        #if not, is colony ii QR but becomes QL?
-            } else if (queen_survival[ii] == 0 && population[ii,number_alleles + 4] == 1){ 
-
-                                        #copy colony ii exactly except for its QR/QL indicator column
-                new_population[swarm_counter,1:(number_alleles + 3)] <- population[ii,1:(number_alleles + 3)] 
-                                        #change colony indicator to QL
-                new_population[swarm_counter,number_alleles + 4] <- 0
-
-                ## store which colony this entry is derived from
-                population_original_queen_index[swarm_counter] <- ii
-
-                swarm_counter <- swarm_counter + 1 
-                
-                                        #QR colony produces drones
-                distribution_alleles[population[ii,1:2]] <- distribution_alleles[population[ii,1:2]] + 
-                    (population[ii,number_alleles + 3] * 0.5) 
-                
-                                        # if not, is colony ii QR but will die next generation?
-            } else if (queen_survival[ii] == -1 && population[ii,number_alleles + 4] == 1){
-                
-                                        #if so, it produces drones but it will not persist in the new population
-                
-                ## QR colony produces drones
-                distribution_alleles[population[ii,1:2]] <- distribution_alleles[population[ii,1:2]] + 
-                    (population[ii,number_alleles + 3] * 0.5) 
-                
-                                        #or is the colony QL?
-            } else if (population[ii,number_alleles + 4] == 0){ 
-                ## QL colonies produce no new colonies but do produce drones
-######
-                ## For each QL colony "row", if the QL colony is a "newly-produced daughter colony that became queenless during her mating flight" then the queen nuclear alleles and the spermathecal contents are not from the dead virgin queen but instead from the virgin queen's mother (since we are assuming that the virgin queen is lost on her mating flight, which means that the queenless workers are actually her mother's daughters. In effect, this means that half of the drone alleles from a QL colony come from the original queen (i.e. the virgin queen's mother) since each worker carries one allele from the original queen's nucleus. The other half come from the original queen's spermatheca since each worker carries one allele from the original queen's spermatheca. If the QL colony was headed by a mated queen that died when finding a new nest, then the workers are derived from the mated queen.
-                ## The differences between the two QL colonies are accounted for elsewhere (i.e. below all QL colonies are treated identically, since each row contains the relevant info (the virgin queen's mother's nuclear/spermatheca for newly QL colonies but the mated queen's nucleus/spermatheca for established queens that die trying to relocate to a new nest)
-
-                ## original queen's nucleus
-                distribution_alleles[population[ii,1:2]] <- distribution_alleles[population[ii,1:2]] + 
-                    (population[ii,number_alleles + 3] * 0.25) ## note the 0.25 instead of 0.5 
-                ## original queen's spermatheca
-                distribution_alleles <- distribution_alleles +  ( (population[ii,number_alleles + 3] * 0.5) * population[ii,3:(number_alleles+2)] ) ## population[ii,3:(number_alleles+2)] is the spermathecal vector that sums to 1, so I need to multiply by this to correctly assign the relative proportion of each. I multiply by 0.5 because the entire thing counts for half (each nuclear allele counts for 0.25, giving 0.5 in total for the nuclear also)
-            }
-            
-                                        #if the iith colony is QR, it can produce daughter swarms (even if it will become dead or QL)
-                                        #if a QR colony produces a non-zero number of daughter colonies
-            if (population[ii,number_alleles + 4] == 1 && swarming_vector[ii] > 0){
-                
-                for (jj in 1:swarming_vector[ii]){ #loop through those daughter colonies
-                                        #first, choose queen alleles for jjth daughter colony of colony ii
-                    
-                                        #randomly choose an allele from previous queen (equal chance of either allele)
-                    new_population[swarm_counter,1] <- population[ii,sample(1:2,1)]
-                    
-                                        #randomly choose an allele from the previous queen's spermatheca (probabilty depends on frequency of sperm in spermatheca)
-                    new_population[swarm_counter,2] <- sample(1:number_alleles,1, prob = population[ii,3:(number_alleles + 2)])
-                    while (new_population[swarm_counter,1] == new_population[swarm_counter,2]){ #if the new queen is homozygous, rechoose
-                        new_population[swarm_counter,1] <- population[ii,sample(1:2,1)] 
-                        new_population[swarm_counter,2] <- sample(1:number_alleles,1, prob = population[ii,3:(number_alleles + 2)])
-                    }
-                    ## store which colony this entry is derived from
-                    population_original_queen_index[swarm_counter] <- ii
-                    
-                    swarm_counter <- swarm_counter + 1
-                    
-                }
-            } #if it's QL, it can't produce daughter swarms 
-        } ## for ends the for 1:N loop
-        
-        distribution_alleles <- distribution_alleles / sum(distribution_alleles)
-        
-                                        #choose which drones each queen mates with 
-        sampled_drones <- 
-            matrix(sample(1:number_alleles,N_new_generation * number_drone_matings,replace = TRUE, 
-                          prob = distribution_alleles), nrow = N_new_generation, ncol = number_drone_matings)
-        
-                                        #for all NEW colonies ONLY:
-        
-                                        #transform drone alleles into proportions - drones mate with queens 
-                                        #calculate colony fitness
-                                        #assign daughter swarms a QR or QL state
-        
 getColonyFitness <- function(colony_ID, number_alleles, population){
 
     return( population[colony_ID, number_alleles + 3] )
@@ -268,33 +141,67 @@ isColonyExtinct <- function(population, number_alleles){
 }
 
 generateNewPopulation <- function(population, number_alleles, number_drone_matings,
-            
-            if (sum(new_population[ii,3:(number_alleles + 2)]) == 0){ # means daughter colony NOT surviving queen
-                ## now I first chose whether the colony goes QR or QL, as this affects where the workers are derived from
-
-                new_population[ii,number_alleles + 4] <- runif(1) < (1 - probability_QL_colony) # 1 if QR, 0 if QL
-
-                if (new_population[ii,number_alleles + 4] == 1){ ## QR
-                    
-                    new_population[ii,3:(number_alleles + 2)] <- tabulate(bin = sampled_drones[ii,],nbins = number_alleles) / number_drone_matings
-                    
-                                        # determine proportion of homozygosity by checking each queen allele and multiplying it by the corresponding drone allele
-                                        # each queen allele contributes half the total homozygosity and total homozygosity is multipled by its cost
-                    new_population[ii,number_alleles + 3] <- 1 - (((0.5 * new_population[ii,new_population[ii,1] + 2]) + 
-                                                                   (0.5 * new_population[ii,new_population[ii,2] + 2])) * cost_homozoygosity)
-                } else { ## QL
-                    ## population[population_original_queen_index[ii], ] gives the vector corresponding to the original queen 
-                    new_population[ii,1:(number_alleles + 3)] <- population[population_original_queen_index[ii], 1:(number_alleles + 3)]
-                    ## tag as QL colony
-                    new_population[ii,number_alleles + 4] <- 0
-                }
-                
-            } # else, do nothing because surviving queens do not mate again
-            
-        }
+                                  prob_queen_survives, cost_homozoygosity, QL_drone_production){
+    
+    
+    ## new population (start with no rows and rbind new colonies as I go)
+    new_population <- matrix(nrow = 0, ncol = 2 + number_alleles + 2)
+    
+    worker_laid_drone_alleles <- numeric(number_alleles)
+    queen_laid_drone_alleles <- numeric(number_alleles)
+    
+    for ( i in 1:NROW(population) ){
         
-        population<- new_population
-        return(population)
+        if ( isColonyQR(colony_ID = i, number_alleles, population) ){ ## is colony QR?
+            ## copy colony i exactly (surviving queen does not remate)
+            new_population <- rbind( new_population, population[i, ] ) 
+            ## does it remain QR? (Queen survival is proportional to colony fitness.) If not, need to change status to QL
+            colony_i_status <- as.integer( testAgainstRandomNumber( prob_queen_survives * getColonyFitness(colony_ID = i, number_alleles, population) ) )
+            new_population <- setColonyQueenStatus(population, number_alleles, colony_ID = NROW(new_population), queen_status = colony_i_status)
+            ## QR colony produces drones (no difference between those remaining QR and those becoming QL--we assume the queen is lost after drone production)
+            queen_laid_drone_alleles <- produceDronesQueenright(population, number_alleles, colony_ID = i, queen_laid_drone_alleles)
+            ## QR colonies can produce daughter queens (even if they become QL because queen is lost after daughter queens have been raised)
+            number_daughter_colonies <- rpois( n = 1, lambda = average_swarms * getColonyFitness(colony_ID = i, number_alleles, population) )
+            
+            if ( as.logical(number_daughter_colonies) ){ ## at least 1 daughter colony is produced
+                ## loop through these daughter colonies
+                for (j in 1:number_daughter_colonies){
+                    ## set up daughter colony (entry complete if it becomes QL; still need spermatheca and colony fitness if remain QR)
+                    new_population <- setupDaughterColony(population, new_population, old_colony_ID = i, number_alleles, prob_queen_survives)
+                }
+            }
+
+        } else if ( !isColonyQR(colony_ID = i, number_alleles, population) ) { ## it is QL (and become so last generation)
+            ## QL colony does not survive (so don't add to new_population) nor does it produce daughter queens
+            ## if QL colony produces drones via workers (proportional to QL_drone_production)
+            ## NOTE THAT HERE I NEED TO ADD A SWITCH TO TURN QL WORKER REPRODUCTION OFF AND ON DEPENDING ON WHETHER WORKERS CAN REPRODUCE
+            ## DETAILS WILL DEPEND ON HOW I DEAL WITH sim_name
+            worker_laid_drone_alleles <- produceDronesQueenless(population, number_alleles, colony_ID = i, worker_laid_drone_alleles, QL_drone_production)
+
+        } else {
+            ## shouldn't execute
+            stopifnot(FALSE)
+        }
+
     }
+
+    ## last step is for daughter queens that survive to mate (giving spermathecal contents), and from this, set colony fitness
+    ## first, get the distribution of drone alleles at the DCA (normalised so it sums to 1)
+    drone_allele_distribution <- (worker_laid_drone_alleles + queen_laid_drone_alleles) / sum(worker_laid_drone_alleles + queen_laid_drone_alleles)
+    stopifnot( sum(drone_allele_distribution) == 1)
+    
+    ## loop through each colony in new_population, find surviving daughter queens (who will mate), and set spermathecal contents and colony fitness
+    for ( i in 1:NROW(new_population) ){
+
+        if ( sum( new_population[i, 3:(number_alleles + 3)] ) == 0 ){ ## surviving daughter queens
+            new_population <- chooseDroneAlleles(new_population, colony_ID = i, number_alleles, number_drone_matings, drone_allele_distribution)
+            new_population <- calculateColonyFitness(new_population, colony_ID = i, number_alleles, cost_homozygosity)
+        }
+
+    }
+
+    ## in addition to population, I also want to return the drone distributions (for analysis)
+    list_output <- list(new_population, queen_laid_drone_alleles, worker_laid_drone_alleles)
+    return(list_output)
     
 }
